9d93746d059a5659ce91380298cc5c24b7693e1c
gabriel <777canaan@gmail.com>
2017-12-13 12:02:59 +1300

    remove function added

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,22 +1,30 @@
 console.log('starting notes.js');
 const fs = require('fs')
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json')
+    return JSON.parse(notesString)
+  } catch (e) {
+    return []
+  }
+}
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+}
+
 var addNote = (title, body) => {
-  var notes = []
+  var notes = fetchNotes()
   var note = {
     title,
     body
   }
-  try {
-    var notesString = fs.readFileSync('notes-data.json')
-    notes = JSON.parse(notesString)
-  } catch (e) {
-
-  }
   var duplicateNotes = notes.filter((note) => note.title === title)
   if (duplicateNotes.length === 0) {
     notes.push(note)
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+    saveNotes(notes)
+    return note
   }
 }
 
@@ -27,7 +35,10 @@ var readNote = (title) => {
   console.log('Reading note', title);
 }
 var removeNote = (title) => {
-  console.log('Removing note', title);
+  var notes = fetchNotes()
+  var filteredNotes = notes.filter((note) => note.title !== title)
+  saveNotes(filteredNotes)
+  return notes.length !== filteredNotes.length
 }
 
 module.exports = {
